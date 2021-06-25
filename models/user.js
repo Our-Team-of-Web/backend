62b4cba8d3020fb23f9c59dab2c2039c369c305e
Soumya Sen <sensoumya533@gmail.com>
2021-06-26 00:47:25 +0530

    Third commit

--- models/user.js
+++ models/user.js
@@ -1,30 +1,26 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-const userSchema=mongoose.Schema({
- name:{
-  type:String,
-  required:true,
- },
- email:{
-  type:String,
-  required:true,
- },
- password:{
-  type:String,
-  required:true,
- },
- solved:{
-  type:Array,
-  default:[],
- },
- added:{
-  type:Array,
-  default:[],
- },
- createdAt:{
-  type:Date,
-  default:Date.now(),
- }
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  solved: {
+    type: Array,
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 })
 
-module.exports=mongoose.model("User",userSchema)
+module.exports = mongoose.model('User', userSchema)
